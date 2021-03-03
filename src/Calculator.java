f7b378e514bc0bb7e33f5ce066d2b5c9427f392e
Daria Guskova <dasha.gusckowa@gmail.com>
2021-03-03 23:48:22 +0500

    2nd Commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,56 +1,66 @@
 import java.util.Scanner;
 
 
-public class Operation {
+public class Calculator {
     public static void main(String[] args) {
 
         while (true)    // делаю программу цикличной
 
-            {
-                // прошу у пользователя ввод и проверяю его на правильность методом Valid
+        {
+            // прошу у пользователя ввод и проверяю его на правильность методом Valid
             Scanner in = new Scanner(System.in);
             String operationToExecute;
             System.out.println("Введите операцию:");
+
+
             do {
                 operationToExecute = in.nextLine();
+                if (operationToExecute.equals("stop")) {
+                    System.exit(1); // стоп-слово, останавливающее программу
+                }
             } while (!Valid(operationToExecute));
 
 
             // из введенной строки делаю массив строк, в котором 3 строки: первая - это первая цифра, вторая - это операнд, третья - вторая цифра
             String[] operationParts = operationToExecute.split("(?<=\\d)(?=\\D)|(?<=\\D)(?=\\d)");
 
+            double numb1 = Double.parseDouble(operationParts[0]);
+            double numb2 = Double.parseDouble(operationParts[2]);
+
             // по операнду (второй строке в массиве строк) определяю, какой метод должен использовать калькулятор
-            if (operationParts[1] == "+") {
+            if (operationParts[1].equals("+")) {
                 Sum sum = new Sum();
-                sum.Execute(operationParts);
+                sum.execute(numb1, numb2);
             }
 
-            if (operationParts[1] == "-") {
+            if (operationParts[1].equals("-")) {
                 Minus minus = new Minus();
-                minus.Execute(operationParts);
+                minus.execute(numb1, numb2);
             }
 
-            if (operationParts[1] == "/") {
+            if (operationParts[1].equals("/")) {
                 Divide divide = new Divide();
-                divide.Execute(operationParts);
+                divide.execute(numb1, numb2);
             }
 
-            if (operationParts[1] == "*") {
+            if (operationParts[1].equals("*")) {
                 Multiply multiply = new Multiply();
-                multiply.Execute(operationParts);
+                multiply.execute(numb1, numb2);
             }
 
 
         }
     }
 
+
     public static boolean Valid(String operationToExecute) {
+
 
         // проверяю правильность ввода
         for (int i = 0; i <= operationToExecute.length() - 1; i++) {
             char c = operationToExecute.charAt(i);
             if (!(Character.isDigit(c)) && (c != '+') && (c != '-') && (c != '/') && (c != '*')) {
-                System.out.println("Введите операцию правильно (Пример: 17+4 или 21-13, без пробелов):");
+                System.out.println("Введите операцию правильно (Пример: 17+4 или 21-13, без пробелов, только целочисленные значения):");
                 return false;
             }
         }
@@ -59,61 +69,46 @@ public class Operation {
 }
 
 
-class Operation1 {
+class Operation {
 
-    int Execute(String[] operationParts) {
-        int numb1 = Integer.parseInt(operationParts[0]);
-        int numb2 = Integer.parseInt(operationParts[2]);
+    void execute(double numb1, double numb2) {
         System.out.println("Результат: " + numb1 + numb2);
-        return numb1 + numb2;
     }
 }
 
-class Sum extends Operation1 {
+class Sum extends Operation {
 
 
     @Override
-    int Execute(String[] operationParts) {
-        int numb1 = Integer.parseInt(operationParts[0]);
-        int numb2 = Integer.parseInt(operationParts[2]);
+    void execute(double numb1, double numb2) {
         System.out.println("Результат: " + (numb1 + numb2));
-        return numb1 + numb2;
     }
 
 }
 
-class Minus extends Operation1 {
+class Minus extends Operation {
 
     @Override
-    int Execute(String[] operationParts) {
-        int numb1 = Integer.parseInt(operationParts[0]);
-        int numb2 = Integer.parseInt(operationParts[2]);
+    void execute(double numb1, double numb2) {
         System.out.println("Результат: " + (numb1 - numb2));
-        return numb1 - numb2;
     }
 
 }
 
-class Multiply extends Operation1 {
+class Multiply extends Operation {
 
     @Override
-    int Execute(String[] operationParts) {
-        int numb1 = Integer.parseInt(operationParts[0]);
-        int numb2 = Integer.parseInt(operationParts[2]);
+    void execute(double numb1, double numb2) {
         System.out.println("Результат: " + (numb1 * numb2));
-        return numb1 * numb2;
     }
 
 }
 
-class Divide extends Operation1 {
+class Divide extends Operation {
 
     @Override
-    int Execute(String[] operationParts) {
-        int numb1 = Integer.parseInt(operationParts[0]);
-        int numb2 = Integer.parseInt(operationParts[2]);
+    void execute(double numb1, double numb2) {
         System.out.println("Результат: " + (numb1 / numb2));
-        return numb1 / numb2;
     }
 
 }
